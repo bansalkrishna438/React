5ce6e09c61e8775ffc5c095e2b2303215e035811
kbansalIngenuity <139536798+kbansalIngenuity@users.noreply.github.com>
2024-06-24 12:10:32 +0530

    tween complete

--- 14-gsapproject/src/App.js
+++ 14-gsapproject/src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 const AnimatedComponent = () => {
   const boxRef = useRef(null);
@@ -12,15 +13,21 @@ const AnimatedComponent = () => {
   const boxRef6 = useRef(null);
   const boxRef7 = useRef(null);
   const boxRef8 = useRef(null);
+  const boxRef9 = useRef(null);
+  const boxRef10 = useRef(null);
+  const boxRef11 = useRef(null);
 
   useEffect(() => {
     // Animate the box element
-    gsap.to(boxRef.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
-    gsap.to(boxRef1.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
-    gsap.to(boxRef2.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
-    gsap.to(boxRef3.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
-    gsap.to(boxRef4.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
-    gsap.to(boxRef5.current, { rotation: 360, y: 100, yoyo:true,duration: 0.8,repeat:-1 });
+    gsap.to(boxRef9.current, { rotation: 360, y: -100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef10.current, { rotation: 360, y: -100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef11.current, { rotation: 360, y: -100, yoyo:true,duration: 0.8,repeat:-1 });
+    gsap.to(boxRef.current, { rotation: 360, x: 100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef1.current, { rotation: 360, x: 100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef2.current, { rotation: 360, x: 100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef3.current, { rotation: 360, x: -100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef4.current, { rotation: 360, x: -100,yoyo:true, duration: 0.8,repeat:-1 });
+    gsap.to(boxRef5.current, { rotation: 360, x: -100, yoyo:true,duration: 0.8,repeat:-1 });
     gsap.to(boxRef6.current, { rotation: 360, y: 100, yoyo:true,duration: 0.8,repeat:-1 });
     gsap.to(boxRef7.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
     gsap.to(boxRef8.current, { rotation: 360, y: 100,yoyo:true, duration: 0.8,repeat:-1 });
@@ -114,6 +121,37 @@ const AnimatedComponent = () => {
       >Round<br/>Reactangle</div>
        <div  className='flex text-center justify-center items-center ver'
         ref={boxRef8}
+        style={{
+          width: '100px',
+          height: '100px',
+          backgroundColor: 'red',
+          margin: '50px',
+        }}
+      >Reactangle</div>
+    </div>
+    <div className='flex mt-0 p-0 justify-center items-center content-center'>
+      <div   className='flex text-center justify-center items-center ver'
+        ref={boxRef9}
+        style={{
+          width: '100px',
+          height: '100px',
+          borderRadius:"50px",
+          backgroundColor: 'red',
+          margin: '50px',
+        }}
+      >circle</div>
+       <div   className='flex text-center justify-center items-center ver'
+        ref={boxRef10}
+        style={{
+          width: '100px',
+          height: '100px',
+          borderRadius:"20px",
+          backgroundColor: 'red',
+          margin: '50px',
+        }}
+      >Round<br/>Reactangle</div>
+       <div  className='flex text-center justify-center items-center ver'
+        ref={boxRef11}
         style={{
           width: '100px',
           height: '100px',
